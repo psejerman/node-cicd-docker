46f556030181c612e96b711004857efdd8aa2f3d
Peter Sejerman <p.sejerman@gmail.com>
2024-02-21 21:41:44 +0100

    httpcode error response test

--- app/server.js
+++ app/server.js
@@ -6,7 +6,10 @@ server.use(bodyParser.json());
 const Database = require('./db');
 
 const productRouter = require('./controllers/product.controller');
+const errorRouter = require('./controllers/errors-test.controller');
+
 server.use('/products', productRouter);
+server.use('/errors', errorRouter);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
