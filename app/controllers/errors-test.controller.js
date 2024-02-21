0f77cc5d29061889e05b6945d0d9f0d73751500f
psejerman <75121275+psejerman@users.noreply.github.com>
2024-02-21 21:47:11 +0100

    Update errors-test.controller.js

--- app/controllers/errors-test.controller.js
+++ app/controllers/errors-test.controller.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/401', async(req, res) => {
     try {
-        res.status(401);
+        res.sendStatus(401);
     } catch (e) {
         res.status(500).json({message: e.message});
     }
